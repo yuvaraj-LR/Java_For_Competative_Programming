1f6ad9b1e7cff6876f48960e7df216ba399ff35d
yuvaraj-LR <yuvarajrama121@gmail.com>
2023-04-14 18:55:18 +0530

    OOPs in Java

--- Java_For_Competative_Programming-master/Rough/src/SourceCode/OOPs_08/OOP_2/Packages/a/b/Greeting.java
+++
@@ -1,7 +0,0 @@
-package SourceCode.OOPs_08.OOP_2.Packages.a.b;
-
-public class Greeting {
-    public static void greeting() {
-        System.out.println("Good Morning!!! EveryOne.");
-    }
-}
--- Java_For_Competative_Programming-master/SourceCode/FirstJavaProgram_01/FirstJavaProgram.java
+++
@@ -1,7 +0,0 @@
-package SourceCode.FirstJavaProgram_01;
-
-public class FirstJavaProgram {
-    public static void main(String[] args) {
-        System.out.println("Hello World.");
-    }
-}
